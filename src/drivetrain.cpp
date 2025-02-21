8b2379a91549cfea189e1820803a028a38823a41
Alex Schott <schott.166@osu.edu>
2025-02-21 12:08:08 -0500

    Add driving straight code

--- src/drivetrain.cpp
+++ src/drivetrain.cpp
@@ -1,5 +1,7 @@
 #include "drivetrain.h"
 
+#include <FEHMotor.h>
+
 #include "hardware.h"
 
 void Drivetrain::stop() {
@@ -16,4 +18,37 @@ void Drivetrain::rotateClockwise(float speed) {
 
 void Drivetrain::rotateCounterClockwise(float speed) {
     rotateClockwise(-speed);
+}
+
+void Drivetrain::driveAxis(Axis axis, float speed, bool strafe) {
+    FEHMotor *axialMotor;
+    FEHMotor *leftMotor;
+    FEHMotor *rightMotor;
+
+    switch (axis) {
+        case forward:
+            axialMotor = &Hardware::rearMotor;
+            leftMotor = &Hardware::leftMotor;
+            rightMotor = &Hardware::rightMotor;
+            break;
+        case left:
+            axialMotor = &Hardware::leftMotor;
+            leftMotor = &Hardware::rearMotor;
+            rightMotor = &Hardware::rightMotor;
+        case right:
+            axialMotor = &Hardware::rightMotor;
+            leftMotor = &Hardware::rearMotor;
+            rightMotor = &Hardware::leftMotor;
+            break;
+    }
+
+    // I took this from https://en.wikipedia.org/wiki/Kiwi_drive#Motion
+    if (strafe) {
+        axialMotor->SetPercent(speed);
+        leftMotor->SetPercent(-speed / 2);
+        rightMotor->SetPercent(-speed / 2);
+    } else {
+        leftMotor->SetPercent(speed);
+        rightMotor->SetPercent(-speed);
+    }
 }
--- src/drivetrain.h
+++ src/drivetrain.h
@@ -1,8 +1,11 @@
 class Drivetrain {
    public:
+    enum Axis { forward, left, right };
+
     void stop();
     void rotateClockwise(float speed);
     void rotateCounterClockwise(float speed);
+    void driveAxis(Axis axis, float speed, bool strafe = false);
 };
 
 extern Drivetrain drivetrain;
