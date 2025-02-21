1e9777a1565b99b711f1e78d7061848824b36091
Alex Schott <schott.166@osu.edu>
2025-02-21 11:21:06 -0500

    Add hardware definitions

---
+++ src/hardware.h
@@ -0,0 +1,10 @@
+#include <FEHIO.h>
+#include <FEHMotor.h>
+
+namespace Hardware {
+
+FEHMotor rearMotor(FEHMotor::Motor0, 9.0);
+FEHMotor leftMotor(FEHMotor::Motor0, 9.0);
+FEHMotor rightMotor(FEHMotor::Motor0, 9.0);
+
+};   // namespace Hardware
