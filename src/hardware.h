d7a1b8db64c41f6a12b0a0aa84341f1dcc692020
Alex Schott <schott.166@osu.edu>
2025-02-26 11:25:56 -0500

    Add motor definitions

--- src/hardware.h
+++ src/hardware.h
@@ -3,8 +3,8 @@
 
 namespace Hardware {
 
-FEHMotor rearMotor(FEHMotor::Motor0, 9.0);
+FEHMotor rearMotor(FEHMotor::Motor2, 9.0);
 FEHMotor leftMotor(FEHMotor::Motor0, 9.0);
-FEHMotor rightMotor(FEHMotor::Motor0, 9.0);
+FEHMotor rightMotor(FEHMotor::Motor1, 9.0);
 
 };   // namespace Hardware
