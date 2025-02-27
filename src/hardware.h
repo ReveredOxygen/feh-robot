976d6c5a636b98f27786a3466eef04b7559d32eb
Alex Schott <schott.166@osu.edu>
2025-02-27 16:56:38 -0500

    Add servo definitions

--- src/hardware.h
+++ src/hardware.h
@@ -7,4 +7,8 @@ FEHMotor rearMotor(FEHMotor::Motor1, 9.0);
 FEHMotor leftMotor(FEHMotor::Motor3, 9.0);
 FEHMotor rightMotor(FEHMotor::Motor2, 9.0);
 
+DigitalEncoder rearEncoder(FEHIO::P0_1);
+DigitalEncoder leftEncoder(FEHIO::P3_0);
+DigitalEncoder rightEncoder(FEHIO::P0_0);
+
 };   // namespace Hardware
